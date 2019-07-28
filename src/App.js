16f268c74a7db1051798d32e44439fded37be6fe
Bluuax <Bluuax@users.noreply.github.com>
2019-07-28 23:12:23 +0200

    Back to spaces

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-			<p>Gistory</p>
-		</div>
-	);
+  return (
+    <div className="App">
+      <p>Gistory</p>
+    </div>
+  );
 }
 
 export default App;
