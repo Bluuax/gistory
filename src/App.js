8811ca0cd20b3586a23a9b7c73dac3f893fd9440
Bluuax <Bluuax@users.noreply.github.com>
2019-07-28 22:47:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-	return <p>Gistory</p>;
+	return (
+		<div className="App">
+			<p>Gistory</p>
+		</div>
+	);
 }
 
 export default App;
