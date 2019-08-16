f63273c36e67bbcb0e4c7ba3960b715ee5d83220
Bluuax <Bluuax@users.noreply.github.com>
2019-08-16 09:57:09 +0200

    Add structure, update to React 16.9

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Landing from './Landing';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Gistory</p>
+      <Landing />
     </div>
   );
 }
---
+++ src/Landing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Gistory</h1>
+        <h3>Let's have a look</h3>
+        <input type="text"/>
+      </div>
+    )
+  }
+}
+
+export default Landing
