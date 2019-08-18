abe3788e61ffc70ba85a5dd31fc874a698382f88
Bluuax <Bluuax@users.noreply.github.com>
2019-08-18 11:04:18 +0200

    Small fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Header';
 import Landing from './Landing';
-import History from './history/History';
 import Footer from './Footer';
 import './App.css';
 
