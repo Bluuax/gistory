074b7ecd7f41aa0f729a3ef34c797c0f29dab6b7
Bluuax <Bluuax@users.noreply.github.com>
2019-09-28 10:13:53 +0200

    Add easter egg

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { useReducer } from 'react';
+import { Context, initialState, reducer } from './common/store';
 import { Switch, Route } from 'react-router-dom';
+import Konami from 'react-konami-code';
+import axios from 'axios';
 import Navbar from './Navbar';
 import Search from './Search';
 import History from './history/History';
 import Authentication from './common/Authentication';
 import About from './About';
-import NotFound from './NotFound';
+import { message } from 'antd';
 import './App.css';
-
-import { Context, initialState, reducer } from './common/store';
 
 function App() {
   const [store, dispatch] = useReducer(reducer, initialState);
+
+  const easterEgg = async () => {
+    try {
+      const zenMessage = await axios.get('https://api.github.com/zen');
+      message.info(zenMessage.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <Context.Provider value={{ store, dispatch }}>
       <div className="App">
@@ -19,12 +30,13 @@ function App() {
           <Navbar />
         </header>
         <Switch>
-          <Route exact path="/" component={Search}></Route>
-          <Route exact path="/history" component={History}></Route>
-          <Route exact path="/tutorial" component={Authentication}></Route>
-          <Route exact path="/about" component={About}></Route>
-          <Route component={NotFound}></Route>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/history" component={History} />
+          <Route exact path="/tutorial" component={Authentication} />
+          <Route exact path="/about" component={About} />
+          <Route component={Search} />
         </Switch>
+        <Konami action={easterEgg} />
       </div>
     </Context.Provider>
   );
