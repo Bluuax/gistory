32a9a3aa498cd02c9fee051768baf56c4bbd3add
Bluuax <Bluuax@users.noreply.github.com>
2019-10-05 16:16:16 +0200

    Update Card styling

--- src/history/Card.js
+++ src/history/Card.js
@@ -7,19 +7,27 @@ function Card(props) {
     <div className="Card" style={{ backgroundColor: props.color }}>
       <h2 className="Card-title">{props.title}</h2>
 
-      {/* TODO */}
+      {/* TODO: Null überprüfungen */}
       {props.title === 'Info' && (
         <div className="Card-content">
-          <p>File Name: {props.data.name}</p>
-          <p>Commit Message: {props.data.commit.message}</p>
-          <p>Commit Date/Time: {formattedDateTime(new Date(props.data.commit.author.date))}</p>
-          <p>
-            Author:{' '}
+          <div className="Card-content-section">
+            <span className="Card-description">File: </span>
+            <span>{props.data.name}</span>
+          </div>
+          <div className="Card-content-section">
+            <span className="Card-description">Message: </span>
+            <span>{props.data.commit.message}</span>
+          </div>
+          <div className="Card-content-section">
+            <span className="Card-description">Timestamp: </span>
+            <span> {formattedDateTime(new Date(props.data.commit.author.date))}</span>
+          </div>
+          <div>
+            <span className="Card-description">Author: </span>
             <a href={props.data.author && props.data.author.html_url} target="_blank" rel="noopener noreferrer">
               {props.data.commit.author.name}
             </a>
-          </p>{' '}
-          {/* Link zu GitHub User */}
+          </div>
         </div>
       )}
     </div>
