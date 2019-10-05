d5da03e0f2f65f69740e64c3b45265ba98c8e632
Bluuax <Bluuax@users.noreply.github.com>
2019-10-05 23:07:42 +0200

    Add chart.js, small refactor

--- src/history/Code.js
+++ src/history/Code.js
@@ -1,41 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import { Base64 } from 'js-base64';
 import theme from 'prism-react-renderer/themes/github';
 import './Code.css';
 
-export class Code extends Component {
-  render() {
-    return (
-      <div className="Code">
-        {/* TODO: Refactor code */}
-        <Highlight
-          {...defaultProps}
-          theme={theme}
-          code={Base64.decode(this.props.content)}
-          language={this.props.language}
-        >
-          {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre className={className} style={style}>
-              <code>
-                {tokens.map((line, i) => (
-                  <div
-                    {...getLineProps({ line, key: `${i}${line.map(item => item.content)}` })}
-                    className="Code-line-animation"
-                  >
-                    <div className="line-number">{i + 1}</div>
-                    {line.map((token, key) => (
-                      <span {...getTokenProps({ token, key })} />
-                    ))}
-                  </div>
-                ))}
-              </code>
-            </pre>
-          )}
-        </Highlight>
-      </div>
-    );
-  }
+function Code(props) {
+  return (
+    <div className="Code">
+      <Highlight {...defaultProps} theme={theme} code={Base64.decode(props.content)} language={props.language}>
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            <code>
+              {tokens.map((line, i) => (
+                <div
+                  {...getLineProps({ line, key: `${i}${line.map(item => item.content)}` })}
+                  className="Code-line-animation"
+                >
+                  <div className="line-number">{i + 1}</div>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </code>
+          </pre>
+        )}
+      </Highlight>
+    </div>
+  );
 }
 
 export default Code;
