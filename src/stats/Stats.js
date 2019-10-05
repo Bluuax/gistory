5e16485ff2ce469f2295de13838441b915a82d65
Bluuax <Bluuax@users.noreply.github.com>
2019-10-05 23:52:44 +0200

    Add prototype of Stats

--- src/stats/Stats.js
+++ src/stats/Stats.js
@@ -1,17 +1,57 @@
 import React, { useContext } from 'react';
 import { Context } from '../common/store';
 import { Redirect } from 'react-router-dom';
+import { Doughnut } from 'react-chartjs-2';
 import './Stats.css';
 
 function Stats() {
   const { store } = useContext(Context);
+
+  // TODO: 10 Farben vorgeben, rest generieren lassen
+  const randomColor = () => {
+    return `${Math.floor(Math.random() * 255)}, 
+    ${Math.floor(Math.random() * 255)}, 
+    ${Math.floor(Math.random() * 255)}, 0.6`;
+  };
+
   return (
     <>
       {store.url === '' ? (
         <Redirect to="/" />
       ) : (
         <div className="Stats">
-          <h1>Stats</h1>
+          <Doughnut
+            data={{
+              labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+              datasets: [
+                {
+                  label: 'Population',
+                  data: [617594, 181045, 153060, 106519, 105162, 95072],
+                  backgroundColor: [
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(153, 102, 255, 0.6)',
+                    'rgba(255, 159, 64, 0.6)',
+                    'rgba(255, 99, 132, 0.6)',
+                    `rgba(${randomColor()})`
+                  ]
+                }
+              ]
+            }}
+            options={{
+              title: {
+                display: true,
+                text: 'Amount of commits',
+                fontSize: 40
+              },
+              legend: {
+                display: true,
+                position: 'right'
+              }
+            }}
+          />
         </div>
       )}
     </>
