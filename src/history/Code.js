884e0bf550a2c610c2d66a000a6df1484279b3ba
Bluuax <Bluuax@users.noreply.github.com>
2019-10-06 13:12:36 +0200

    Update Code

--- src/history/Code.js
+++ src/history/Code.js
@@ -6,22 +6,20 @@ import './Code.css';
 
 function Code(props) {
   return (
-    <div className="Code">
-      <Highlight {...defaultProps} theme={theme} code={Base64.decode(props.content)} language={props.language}>
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={style}>
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: `${i}${line.map(item => item.content)}` })} className="Code-animation">
-                <span className="line-number">{i + 1}</span>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
-          </pre>
-        )}
-      </Highlight>
-    </div>
+    <Highlight {...defaultProps} theme={theme} code={Base64.decode(props.content)} language={props.language}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: `${i}${line.map(item => item.content)}` })} className="Code-animation">
+              <span className="line-number">{i + 1}</span>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
   );
 }
 
